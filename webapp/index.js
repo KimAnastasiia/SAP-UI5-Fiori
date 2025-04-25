799fbc310df7fd9cb74f569ea9d953530812ae39
Anastasiia <anastasiakim2789@gmail.com>
2025-04-25 14:48:54 +0200

    XML Views

--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,17 @@
 sap.ui.define([
-"sap/m/Text"
-], function (Text){
+    "sap/ui/core/mvc/XMLView"
+
+], function (XMLView) {
     "use strict";
 
+    XMLView.create({
 
-    new Text({
-        text:  "Hello UI5!"
-    }).placeAt("content")
+        viewName:"sap.ui.demo.walkthrough.view.App"
 
+    }).then(function (oView) {
+
+        oView.placeAt("content");
+
+
+    })
 });
