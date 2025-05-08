1fc893946557b3b82e3526b1d00240f87b92d0a8
Anastasiia <anastasiakim2789@gmail.com>
2025-05-08 17:14:53 +0200

    Component Configuration

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,27 +3,21 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
-], function (Controller, MessageToast, JSONModel) {
+    "sap/ui/model/resource/ResourceModel",
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
 
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
 
-        onInit: function () {
-            var oData = {
-                recipient: {
-                    name: "UI5",
-
-                }
-            }
-            var oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-        },
-
         onShowHello: function () {
+            //read msg from i18n model
+            let oBundle = this.getView().getModel("i18n").getResourceBundle();
+            let sRecipient = this.getView().getModel().getProperty("/recipient/name");
             //show a native or vanilla JS alert
+            let sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
-            MessageToast.show("Hello there!")
+            MessageToast.show(sMsg)
         }
 
     });
