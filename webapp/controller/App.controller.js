0ff14eef04e095ef8ffc2f62c3eb4ae22cbc48a4
Anastasiia <anastasiakim2789@gmail.com>
2025-05-09 18:43:32 +0200

    Translatable Texts( 18 internationalization, 8)

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,7 +3,8 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
-], function (Controller, MessageToast, JSONModel) {
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
 
     "use strict";
 
@@ -18,12 +19,22 @@ sap.ui.define([
             }
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+            // set i18n model on view
+            var i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+                supportedLocales: [""],
+                fallbackLocale: ""
+            });
+            this.getView().setModel(i18nModel,"i18n");
         },
 
         onShowHello: function () {
+            // read msg from i18n model
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
             //show a native or vanilla JS alert
-
-            MessageToast.show("Hello there!")
+            MessageToast.show(sMsg)
         }
 
     });
