a37ab5a37d86f12c30b5a4df3680e9b74525438f
Anastasiia <anastasiakim2789@gmail.com>
2025-05-13 12:54:53 +0200

    Dialogs and Fragments, Fragment Callbacks (notification window for reuse in ur prgrm. 16, 17)

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -3,8 +3,9 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (Controller, MessageToast, JSONModel, ResourceModel) {
+    "sap/ui/model/resource/ResourceModel",
+    "sap/ui/core/Fragment"
+], function (Controller, MessageToast, JSONModel, ResourceModel, Fragment) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
         onShowHello: function () {
@@ -14,6 +15,25 @@ sap.ui.define([
             var sMsg = oBundle.getText("helloMsg", [sRecipient]);
             //show a native or vanilla JS alert
             MessageToast.show(sMsg)
+        },
+        onOpenDialog: function () {
+            let oView = this.getView();
+            if(!this.byId("helloDialog")){
+                Fragment.load({
+                    id: oView.getId(),
+                    name: "sap.ui.demo.walkthrough.view.HelloDialog",
+                    controller: this
+                }).then(function(oDialog){
+                    oView.addDependent(oDialog);
+                    oDialog.open();
+                })
+            }else{
+                this.byId("helloDialog").open();
+            }
+        },
+        onCloseDialog : function(){
+            this.byId("helloDialog").close();
+
         }
     })
 })
