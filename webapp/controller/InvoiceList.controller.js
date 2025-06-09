fb2183d9620b2437b568d8a38ef794471d696acc
Anastasiia <anastasiakim2789@gmail.com>
2025-06-09 11:25:38 +0200

    Filter input (24 Filtering)

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,16 +1,29 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-     "sap/ui/model/json/JSONModel",
-     "../model/formatter"
-], function (Controller, JSONModel, formatter) {
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
         formatter: formatter,
-        onInit: function(){
+        onInit: function () {
             let oViewModel = new JSONModel({
                 currency: "EUR"
             });
             this.getView().setModel(oViewModel, "view")
+        },
+        onFilterInvoices: function (oEvent) {
+            let aFilter = [];
+            let sQuery = oEvent.getParameter("query");
+            if (sQuery) {
+                aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery))
+            }
+
+            let oList = this.byId("invoiceList");
+            let oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
         }
     })
 
