da58a2770f911d1cc0dbcf340fd23790686613d2
Anastasiia <anastasiakim2789@gmail.com>
2025-06-11 13:54:20 +0200

    26: Remote OData Service and 27: Mock Server Configuration

--- webapp/component.js
+++ webapp/component.js
@@ -5,64 +5,33 @@ sap.ui.define([
     "./controller/HelloDialog"
 ], function (UIComponent, JSONModel, ResourceModel, HelloDialog) {
     "use strict";
-    return UIComponent.extend("sap.ui.walkthrough.Component", {
+    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
         metadata: {
-            rootView: {
-                "viewName": "sap.ui.demo.walkthrough.view.App",
-                "type": "XML",
-                "async": true,
-                "id": "app"
-            },
-            "models": {
-                "miModelo": {
-                    "type": "sap.ui.model.json.JSONModel",
-                    "settings": {
-                        "data": {
-                            "miDato": "Mi casa"
-                        }
-                    }
-                },
-                "invoice": {
-                    "type": "sap.ui.model.json.JSONModel",
-                    "uri": "/Invoices.json"
-                },
- 
-            }  
+            manifest: "json"
         },
         init: function () {
-
-            // kill the init function of parent
+            // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
-            // Set data models
+            // set data models
             var oData = {
                 recipient: {
-                    name: "UI5",
-
+                    name: "UI5"
                 }
-            }
+            };
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
 
-            // set i18n model
-            var i18nModel = new ResourceModel({
-                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
-                supportedLocales: [""],
-                fallbackLocale: ""
-            });
-            this.setModel(i18nModel, "i18n");
-            //in this way we call our cusm fucns
+            // set dialog
             this._helloDialog = new HelloDialog(this.getRootControl());
-
-
         },
-        exis: function () {
+
+        exit: function () {
             this._helloDialog.destroy();
             delete this._helloDialog;
-
         },
-        openHelloDialog: function(){
+
+        openHelloDialog : function () {
             this._helloDialog.open();
-            
-        }
-    })
-})
+        } 
+    });
+});
